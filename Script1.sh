78577cb554db0ed8e40783473e1cfaf90e510548
Vagrant <vagrant@machineMaster.gac.local>
2018-04-06 09:17:39 -0500

    These are my updated files

---
+++ Script1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo"Enter Your Filename"
+read filename
+
+if[[ -f $filename ]]
+	echo "the filename will be converted"
+	my $filename $ filname.sh
+	chmod 755 $filename.sh
+else
+	echo"File deosn't exist"
+fi
